48b4d2bc0edbd4654862f3a726fd55432db5f631
Gpower2 <arslanoglou.georgios@gmail.com>
2014-10-20 20:05:02 +0300

    Changed the Assembly Info and Version
    Changed the Assembly Info and Version

--- gSqlUtils/Properties/AssemblyInfo.cs
+++ gSqlUtils/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("gSqlUtils")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A wrapper for System.Data and System.Data.Sql for easy interaction with MS SQL Server")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Gpower2")]
 [assembly: AssemblyProduct("gSqlUtils")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyCopyright("Copyright © Gpower2 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
